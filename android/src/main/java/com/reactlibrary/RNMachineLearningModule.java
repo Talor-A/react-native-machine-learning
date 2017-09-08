19fd73c7e12f0a175ef2a48921541b23b3b5736f
Talor Anderson <ta.anders17@gmail.com>
2017-09-08 12:35:24 -0700

    first working test for android

--- android/src/main/java/com/reactlibrary/RNMachineLearningModule.java
+++ android/src/main/java/com/reactlibrary/RNMachineLearningModule.java
@@ -5,8 +5,14 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
 
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+import android.util.Log;
 import android.widget.Toast;
+
 import java.util.Map;
 import java.util.HashMap; 
 
@@ -14,6 +20,21 @@ public class RNMachineLearningModule extends ReactContextBaseJavaModule {
 
   private static final String DURATION_SHORT_KEY = "SHORT";
   private static final String DURATION_LONG_KEY = "LONG";
+
+  // Load tensorflow_interface native library
+  static {
+    System.loadLibrary("tensorflow_inference");
+  }
+
+  // Constants
+  private static final String MODEL_FILE = "file:///android_asset/optimized_tfdroid.pb";
+  private static final String INPUT_NODE = "I";
+  private static final String OUTPUT_NODE = "O";
+
+  private static final int[] INPUT_SIZE = {1,3};
+
+  // Create TensorFlowInferenceInterface
+  private TensorFlowInferenceInterface inferenceInterface;
 
   public RNMachineLearningModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -33,4 +54,45 @@ public class RNMachineLearningModule extends ReactContextBaseJavaModule {
   public void show(String message, int duration) {
     Toast.makeText(getReactApplicationContext(), message, duration).show();
   }
+
+    // Bridged methods
+    @ReactMethod
+    public void runInference(Float num1, Float num2, Float num3, Promise promise) {
+        initInferenceInterface();
+
+        float[] inputFloats = {num1, num2, num3};
+
+        // Fill input nodes with desired values
+        Log.w("Tensor", "Filling input nodes with: " + inputFloats[0] + inputFloats[1] +
+                inputFloats[2]);
+        inferenceInterface.fillNodeFloat(INPUT_NODE, INPUT_SIZE, inputFloats);
+
+        // run inference for output node (like sess.run())
+
+        inferenceInterface.runInference(new String[] {OUTPUT_NODE});
+
+        WritableArray resultArray = Arguments.createArray();
+        float[] result = {0, 0};
+
+        // read output node value into result
+        inferenceInterface.readNodeFloat(OUTPUT_NODE, result);
+
+        for (float i : result) {
+            double d = i;
+            resultArray.pushDouble(d);
+        }
+        promise.resolve(resultArray);
+    }
+
+    //--------------------------------------------------------------
+    // Internal methods
+
+    public void initInferenceInterface() {
+        inferenceInterface = new TensorFlowInferenceInterface();
+        inferenceInterface.initializeTensorFlow(getCurrentActivity().getAssets(), MODEL_FILE);
+    }
+
+
+
+
 }
